5e2fb770d9a482bdd65444d94c03b03546f7bf94
tom <p2011303349@126.com>
2019-04-27 10:41:00 +0800

    2.0

--- src/main/java/com/qianfeng/service/impl/UserServiceImpl.java
+++ src/main/java/com/qianfeng/service/impl/UserServiceImpl.java
@@ -1,16 +1,22 @@
 package com.qianfeng.service.impl;
 
+import com.qianfeng.dao.Userdao;
 import com.qianfeng.entity.User;
 import com.qianfeng.service.UserService;
 import com.qianfeng.vo.JsonBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 2019/4/27.
  */
+@Service
 public class UserServiceImpl implements UserService{
-
+    @Autowired
+    private Userdao userDao;
     @Override
     public JsonBean login(String phone, String password) {
+
         return null;
     }
 
