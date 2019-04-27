286cbf7c4ea2e0147371e8976780a42004720134
tom <p2011303349@126.com>
2019-04-27 10:33:43 +0800

    2.0

--- src/main/java/com/qianfeng/service/UserService.java
+++ src/main/java/com/qianfeng/service/UserService.java
@@ -1,8 +1,14 @@
 package com.qianfeng.service;
 
+import com.qianfeng.entity.User;
+import com.qianfeng.vo.JsonBean;
+
 /**
  * Created by Administrator on 2019/4/27.
  */
 public interface UserService {
 
+    public JsonBean login(String phone, String password);
+
+    public User findByUsername(String phone);
 }
